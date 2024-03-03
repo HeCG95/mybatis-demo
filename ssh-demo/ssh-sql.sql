dbd39d209edee93b0d1fa3ee2c964c1edebb387c
HeCG <jacktanger@163.com>
2024-03-03 09:31:36 +0800

    [M] add example.

--- ssh-demo/ssh-sql.sql
+++ ssh-demo/ssh-sql.sql
@@ -3,8 +3,8 @@ CREATE TABLE `t_user` (
                           `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
                           `username` varchar(200) COLLATE utf8_bin DEFAULT NULL,
                           `password` varchar(200) COLLATE utf8_bin DEFAULT NULL,
-                          `account` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+                          `account` int(22) COLLATE utf8_bin DEFAULT NULL,
                           PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `t_user` (`username`, `password`, `account`) VALUES ('xt','123','xt'),('xt2','123','xt2');
+INSERT INTO `t_user` (`username`, `password`, `account`) VALUES ('xt','123',123),('xt2','456',456);
